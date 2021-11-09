64d3fed22edce04a6b8225d354352ea3a9131ce0
Lylant <pphantasm@gmail.com>
2021-11-09 17:41:49 +1100

    Add methods "instantiateExpense", "loadingExpenses"

--- src/PropertyManager/Main.java
+++ src/PropertyManager/Main.java
@@ -34,6 +34,9 @@ public class Main {
 
         if (option != 0)
             option = loadingProperties();
+        if (option != 0)
+            option = loadingExpenses();
+
     }
 
 
@@ -82,6 +85,27 @@ public class Main {
         // return the reference of the new instance of Property
         return newProperty;
     }
+
+    /**
+     * Instantiate a new Expense object.
+     * The data fields of the new object will be setted with next dataset from the Scanner parameter.
+     *
+     * @param input - Scanner object to read the source file
+     * @return the reference of the new Expense object
+     */
+    static Expense instantiateExpense(Scanner input) {
+        // instantiate a new Expense object
+        Expense newExpense = new Expense();
+
+        newExpense.setPropertyID(input.nextInt());
+        newExpense.setDescription(input.next());
+        newExpense.setCost(input.nextDouble());
+        newExpense.setDate(input.next());
+
+        // return the reference of the new instance of Expense
+        return newExpense;
+    }
+
 
     /**
      * Loading the clients records from the source file. The records will be stored in the global
@@ -161,6 +185,48 @@ public class Main {
         // instantiate objects of Client and store them in the arraylist
         while(inputFile.hasNext()) {
             properties.add(instantiateProperty(inputFile));
+        }
+
+        // successfully loading completed
+        return -1;
+    }
+
+    /**
+     * Loading the expenses records from the source file. The records will be stored in the global
+     * arraylist "expenses". Each Property object in the arraylist represents an individual expense.
+     *
+     * @return the flag of loading status. "-1": successful, "0": failed
+     */
+    static int loadingExpenses() {
+        // initialize Scanner object references
+        Scanner inputFile = null;
+
+        // read the expenses file and instantiate Scanner objects
+        while(!fileExpenses.equals("exit")) {
+            try {
+                File sourceFile = new File(fileExpenses);
+                inputFile = new Scanner(sourceFile);
+                break; // successfully read
+
+            } catch (FileNotFoundException exception) {
+                System.out.println("Error - File not found: " + exception.getMessage());
+                System.out.println("\nEnter the name of the file that is to be read."
+                        + " Type \"exit\" to exit.");
+                System.out.print("File Name?: ");
+                fileExpenses = kb.nextLine(); // get a new file name to be read
+            }
+        }
+
+        // terminate the program if the user entered "exit"
+        if(fileExpenses.equals("exit"))
+            return 0;
+
+        // set delimiters for the Scanner objects as ",", "\n", and "\r\n"
+        inputFile.useDelimiter(",|\n|\r\n");
+
+        // instantiate objects of Client and store them in the arraylist
+        while(inputFile.hasNext()) {
+            expenses.add(instantiateExpense(inputFile));
         }
 
         // successfully loading completed
