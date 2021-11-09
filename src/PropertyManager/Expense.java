2ab62a98c1244b5485a289e5b1630dfeec5f8fa3
Lylant <51321187+lylant@users.noreply.github.com>
2021-11-09 17:28:51 +1100

    Create classes (#1)
    * Create a class "Client"
    * Create a class "Property"
    * Create a class "Expense"
    * Create a class "Rent"

---
+++ src/PropertyManager/Expense.java
@@ -0,0 +1,82 @@
+package PropertyManager;
+
+public class Expense {
+
+    // DATA FIELDS
+    private int propertyID;       // the ID of the property for which the expense was incurred
+    private String description;   // a description of the expense
+    private double cost;          // the cost of the expense
+    private String date;          // the date on which the expense was incurred
+    
+    // As this class is not used for processing date, the date is String datatype for the simplicity
+    // the date is in yyyy-MM-dd format
+
+
+    /**
+     * Non-Argument Constructor
+     */
+    public Expense() {
+        propertyID = -1;
+        description = "";
+        cost = -1;
+        date = "2000-01-01";
+    }
+
+
+    /**
+     * @return the ID of the property for which the expense was incurred
+     */
+    public int getPropertyID() {
+        return propertyID;
+    }
+
+    /**
+     * @return the description of the expense
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @return the cost of the expense
+     */
+    public double getCost() {
+        return cost;
+    }
+
+    /**
+     * @return the date on which the expense was incurred
+     */
+    public String getDate() {
+        return date;
+    }
+
+
+    /**
+     * @param ID - the ID of the property for which the expense was incurred
+     */
+    public void setPropertyID(int ID) {
+        propertyID = ID;
+    }
+
+    /**
+     * @param descr - the description of the expense
+     */
+    public void setDescription(String descr) {
+        description = descr;
+    }
+
+    /**
+     * @param cost - the cost of the expense
+     */
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    /**
+     * @param date - the date on which the expense was incurred
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
