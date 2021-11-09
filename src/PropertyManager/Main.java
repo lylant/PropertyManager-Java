582e3d6054d2b7faaffa3f42d6f5f0b3e3aa2474
Lylant <pphantasm@gmail.com>
2021-11-09 18:32:26 +1100

    Update the method main() to implement the menu functionality

--- src/PropertyManager/Main.java
+++ src/PropertyManager/Main.java
@@ -12,29 +12,64 @@ public class Main {
     // Scanner object for the keyboard input
     static Scanner kb = new Scanner(System.in);
 
-    // default source file names to be read
-    static String fileClients = "clients.txt";
-    static String fileProperties = "properties.txt";
-    static String fileExpenses = "expenses.txt";
-    static String fileRents = "rents.txt";
-
     // instantiate arraylist objects to bind references of each class
     static ArrayList<Client> clients = new ArrayList<>();
     static ArrayList<Property> properties = new ArrayList<>();
     static ArrayList<Expense> expenses = new ArrayList<>();
     static ArrayList<Rent> rents = new ArrayList<>();
 
+    static boolean isSaved = true;
+
 
     public static void main(String[] args) {
 
-        // loading process
+        /*
+         * Prepare the local variables
+         */
+
+        // default source file names to be read
+        String fileClients = "clients.txt";
+        String fileProperties = "properties.txt";
+        String fileExpenses = "expenses.txt";
+        String fileRents = "rents.txt";
+
+        // an array representation of menu options
+        String[] menuOptions = {"Record Rent Collection",
+                "Record Expense",
+                "Generate Portfolio Report",
+                "Save Changes",
+                "Exit the Program"};
+        int menuOptionInput = -1; // the user input for the menu selection
+        int menuOptionExit = 0;   // a digit number to choose the exit program option
+
+
+        /*
+         * Loading Process
+         */
         clients = DatabaseUtility.loadingClients(clients, fileClients);
-        if(clients.size() != 0)
+        if (clients.size() != 0) // skip the next step if the previous loading is failed
             properties = DatabaseUtility.loadingProperties(properties, fileProperties);
-        if(properties.size() != 0)
+        if (properties.size() != 0)
             expenses = DatabaseUtility.loadingExpenses(expenses, fileExpenses);
-        if(expenses.size() != 0)
+        if (expenses.size() != 0)
             rents = DatabaseUtility.loadingRents(rents, fileRents);
 
+        // loading failed: terminate the program
+        if (rents.size() == 0) {
+            System.out.println("Good Bye.");
+            return;
+        }
+
+
+        Menu.displayTitle();
+
+        do {
+            Menu.displayMenu(menuOptions, menuOptionExit);
+            menuOptionInput = Menu.getMenuSelect(menuOptions, menuOptionExit);
+
+            switch (menuOptionInput) {
+            }
+
+        } while (menuOptionInput != 0);
     }
 }
