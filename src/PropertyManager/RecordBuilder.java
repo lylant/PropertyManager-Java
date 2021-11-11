a3f8b679798755d71c17722aca7bba06f8573559
Lylant <pphantasm@gmail.com>
2021-11-11 17:42:10 +1100

    Create a class "RecordBuilder"

---
+++ src/PropertyManager/RecordBuilder.java
@@ -0,0 +1,35 @@
+package PropertyManager;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RecordBuilder {
+
+    public static ArrayList<Rent> recordRent
+            (ArrayList<Property> properties, ArrayList<Rent> rents, Scanner kb) {
+
+        // initialize local variables:
+        ArrayList<Property> searchResults = null; // the arraylist to store the search results
+        int propertyID = -1; // the propertyID of the property the rent collection to be added
+
+        // display the menu welcome message
+        MenuUtility.displayRecordRent();
+
+        // Search the property
+        System.out.println("\nYou need to choose a specific property to add a new rent collection record.");
+        System.out.println("You can find the property by searching the address of the property.");
+        searchResults = SearchUtility.searchPropertiesByAddress(properties, kb);
+
+        if (searchResults.size() > 1) { // the search found 2+ properties, allow the user to select one
+            propertyID = SearchUtility.selectPropertyFromSearch(searchResults, kb);
+        }
+
+        System.out.println(propertyID);
+
+        //else { // the search found only one property, select the property automatically
+        //propertyID = searchResults.get(0).getID();
+        //}
+        
+        return rents;
+    }
+}
