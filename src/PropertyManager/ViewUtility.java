cb97fd0ed711aef0d748af13c7d4a35d4d69e5ff
Lylant <pphantasm@gmail.com>
2021-11-17 18:32:59 +1100

    Add a method "displayReportClient"

--- src/PropertyManager/ViewUtility.java
+++ src/PropertyManager/ViewUtility.java
@@ -1,6 +1,8 @@
 package PropertyManager;
 
 import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -12,7 +14,7 @@ import java.util.Scanner;
  */
 
 public class ViewUtility {
-
+    
     // decimal format for the monetary value;
     private static DecimalFormat df = EnvManager.getDecimalFormat();
 
@@ -28,6 +30,124 @@ public class ViewUtility {
         System.out.println();
     }
 
+
+    /**
+     * Display the portfolio report for the clients. The report shows all property's data each client
+     * has, such as rent, expenses, fee rate, fees, and net value. If the client has no record of 
+     * property or rent/expense, the method will display information message for that. The report's
+     * output will be sorted by client's last name in ascending order.
+     *
+     * @param clients - the arraylist of clients of interested
+     * @param properties - the arraylist of all properties
+     * @param expenses - the arraylist of all expenses
+     * @param rents - the arraylist of all rents
+     */
+    private static void displayReportClient(ArrayList<Client> clients, ArrayList<Property> properties
+            , ArrayList<Expense> expenses, ArrayList<Rent> rents) {
+
+        /*
+         *  initialize local variables:
+         */
+
+        // table formatters
+        int borderLength = 114; // table border length
+        String tableFormatHeader = "%-32s%3s%-50s"; // header formatter
+        String tableFormatBody = "%2s%52s%2s%10s%2s%10s%2s%9s%2s%9s%2s%10s%2s"; // body formatter
+
+        // variables for evaluating the sum values
+        double rentTotal;       // a cumulative value of rents of each property
+        double expensesTotal;   // a cumulative value of expenses of each property
+        double feeTotal;        // a cumulative value of fee of each property
+
+        // current datetime
+        DateTimeFormatter dateFormat = EnvManager.getDateTimeDetailFormatter(); // formatter
+        String datetime = LocalDateTime.now().format(dateFormat); // String containing of the current datetime
+
+        // initial sorting
+        SortUtility.sortClientByLastName(clients);
+        SortUtility.sortPropertyByClientID(properties);
+        ArrayList<Expense> sortedExpenses = SortUtility.sortCopiedExpenseByPropertyID(expenses);
+        ArrayList<Rent> sortedRents = SortUtility.sortCopiedRentsByPropertyID(rents);
+
+
+        // report title
+        drawBorder(borderLength);
+        System.out.println(" [*] PORTFOLIO REPORT");
+        System.out.println("Report Generated: " + datetime);
+
+        // generate the report of each clients
+        for (int i = 0; i < clients.size(); i++) {
+            // initialize the cumulative value
+            rentTotal = 0;
+            expensesTotal = 0;
+            feeTotal = 0;
+
+            // table title
+            System.out.format(tableFormatHeader,
+                    ("\n [*] Client: " + clients.get(i).getFullName()), " / ", clients.get(i).getAddress());
+            System.out.println();
+
+            // no property records, skip the contents
+            if ( !Validator.validateOwnerHasProperty(properties, clients.get(i).getID()) ) {
+                ViewUtility.drawBorder(borderLength);
+                System.out.println(" [!] This client possesses no property.");
+                ViewUtility.drawBorder(borderLength);
+                continue;
+            }
+
+            // no rent/expense records, skip the contents
+            if ( !Validator.validateOwnerHasRentOrExpense
+                    (properties, sortedExpenses, sortedRents, clients.get(i).getID()) ) {
+                ViewUtility.drawBorder(borderLength);
+                System.out.println(" [!] This client has no rental/expense records yet.");
+                ViewUtility.drawBorder(borderLength);
+                continue;
+            }
+
+            // table border
+            ViewUtility.drawBorder(borderLength);
+            // table header
+            System.out.format(tableFormatBody, "| ", "Property", " |", "Rent", " |", "Expenses"
+                    , " |", "Fee Rate", " |", "Fees", " |", "Net", " |");
+            System.out.println();
+            // table border
+            ViewUtility.drawBorder(borderLength);
+            // table body 1: find all properties from the client and print the detail of each property
+            for (int j=0; j < properties.size(); j++) {
+                if (properties.get(j).getClientID() == clients.get(i).getID()) {
+                    // accumulate the sum value
+                    rentTotal += properties.get(j).getTotalRent(sortedRents);
+                    expensesTotal += properties.get(j).getTotalExpenses(sortedExpenses);
+                    feeTotal += properties.get(j).getManagementFee(sortedRents);
+
+                    System.out.format(tableFormatBody, "| ", properties.get(j).getAddress()
+                            , " |", df.format(properties.get(j).getTotalRent(sortedRents))
+                            , " |", df.format(properties.get(j).getTotalExpenses(sortedExpenses))
+                            , " |", df.format(properties.get(j).getManagementRate())
+                            , " |", df.format(properties.get(j).getManagementFee(sortedRents))
+                            , " |", df.format(properties.get(j).getNetBalance(sortedRents, sortedExpenses))
+                            , " |");
+                    System.out.println();
+                }
+                if (properties.get(j).getClientID() > clients.get(i).getID()) // no further record
+                    break;
+            }
+            // table border
+            ViewUtility.drawBorder(borderLength);
+            // table body 2: sum values of all properties
+            System.out.format(tableFormatBody, "| ", "TOTAL"
+                    , " |", df.format(rentTotal)
+                    , " |", df.format(expensesTotal)
+                    , " |", ""
+                    , " |", df.format(feeTotal)
+                    , " |", df.format(rentTotal - expensesTotal - feeTotal)
+                    , " |");
+            System.out.println();
+            // table border
+            ViewUtility.drawBorder(borderLength);
+        }
+    }
+    
 
     /**
      * Display the detail of a specific property
