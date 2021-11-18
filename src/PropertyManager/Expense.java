733d4012e7dc6244283e880df753721c1bcb732b
Lylant <pphantasm@gmail.com>
2021-11-18 21:04:25 +1100

    Extend "MonetaryEvent" to "Expense"

--- src/PropertyManager/Expense.java
+++ src/PropertyManager/Expense.java
@@ -5,30 +5,24 @@ package PropertyManager;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 
-public class Expense {
+public class Expense extends MonetaryEvent {
 
     // decimal format for the monetary value
     private static DecimalFormat df = EnvManager.getDecimalFormat();
 
 
     // DATA FIELDS
-    private int propertyID;       // the ID of the property for which the expense was incurred
     private String description;   // a description of the expense
     private double cost;          // the cost of the expense
-    private String date;          // the date on which the expense was incurred
-    
-    // As this class is not used for processing date, the date is String datatype for the simplicity
-    // the date is in yyyy-MM-dd format
 
 
     /**
      * Non-Argument Constructor
      */
     public Expense() {
-        propertyID = -1;
+        super();
         description = "";
         cost = -1;
-        date = "2000-01-01";
     }
 
     /**
@@ -40,10 +34,9 @@ public class Expense {
      * @param date - the date on which the expense was incurred
      */
     public Expense(int ID, String descr, double cost, String date) {
-        propertyID = ID;
+        super(ID, date);
         description = descr;
         this.cost = cost;
-        this.date = date;
     }
 
 
@@ -51,26 +44,14 @@ public class Expense {
      * @return the summary details of the expense event
      */
     public String toString() {
-        // the hashmaps to convert foreign IDs to meaningful information
-        HashMap<Integer, Client> clientsHashMap = HashMapContainer.getClientsHashMap();
-        HashMap<Integer, Property> propertiesHashMap = HashMapContainer.getPropertiesHashMap();
-
-        String toStr = "Property Address:    " + propertiesHashMap.get(propertyID).getAddress()
-                + "\nProperty Owner:\t     "
-                + clientsHashMap.get(propertiesHashMap.get(propertyID).getClientID()).getFullName()
+        String toStr = "Property Address:    " + super.getPropertyAddress()
+                + "\nProperty Owner:\t     " + super.getPropertyOwnerName()
                 + "\nExpense Description: " + description
                 + "\nMonetary Amount:     $ " + df.format(cost)
-                + "\nCurrent Date:\t     " + date;
+                + "\nCurrent Date:\t     " + super.getDate();
         return toStr;
     }
-    
 
-    /**
-     * @return the ID of the property for which the expense was incurred
-     */
-    public int getPropertyID() {
-        return propertyID;
-    }
 
     /**
      * @return the description of the expense
@@ -87,21 +68,6 @@ public class Expense {
     }
 
     /**
-     * @return the date on which the expense was incurred
-     */
-    public String getDate() {
-        return date;
-    }
-
-
-    /**
-     * @param ID - the ID of the property for which the expense was incurred
-     */
-    public void setPropertyID(int ID) {
-        propertyID = ID;
-    }
-
-    /**
      * @param descr - the description of the expense
      */
     public void setDescription(String descr) {
@@ -113,12 +79,5 @@ public class Expense {
      */
     public void setCost(double cost) {
         this.cost = cost;
-    }
-
-    /**
-     * @param date - the date on which the expense was incurred
-     */
-    public void setDate(String date) {
-        this.date = date;
     }
 }
