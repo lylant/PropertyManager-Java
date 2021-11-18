6724c99513e844aebf54dbb9a1fd8e96fb52a597
Lylant <pphantasm@gmail.com>
2021-11-18 17:26:27 +1100

    Update comments

--- src/PropertyManager/Expense.java
+++ src/PropertyManager/Expense.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class Expense {
 
-    // decimal format for the monetary value;
+    // decimal format for the monetary value
     private static DecimalFormat df = EnvManager.getDecimalFormat();
 
 
